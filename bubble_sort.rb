a62c7fad5d3d83dd06b9f6264b8e52b5f5270578
Osong Agberndifor <agberndifor7@gmail.com>
2020-04-06 09:01:55 +0100

    Add config files and bubble sort code for first function

---
+++ bubble_sort.rb
@@ -0,0 +1,19 @@
+def bubble_sort(arr)
+  arr_len = arr.length
+
+  loop do
+    sorted = false
+    (arr_len -1).times do |i|
+      if arr[i] > arr[i+1]
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        sorted = true
+      end
+    end
+    break if not sorted
+  end
+  arr
+end
+
+list_a = ['hello', 'hi', 'holla', 'hey']
+
+puts bubble_sort(list_a)
